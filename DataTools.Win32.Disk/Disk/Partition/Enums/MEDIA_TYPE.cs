2123971b0763d6997f90eb0e700db9f375e12f0a
Nathaniel Moschkin <ironywrit@gmail.com>
2022-01-31 22:06:02 -0500

    Refactoring of enums and function return results.

---
+++ DataTools.Win32.Disk/Disk/Partition/Enums/MEDIA_TYPE.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTools.Win32.Disk.Partition
+{
+    /// <summary>
+    /// Disk Geometry Media Types
+    /// </summary>
+    /// <remarks></remarks>
+    public enum MEDIA_TYPE
+    {
+        Unknown = 0x0,
+        F5_1Pt2_512 = 0x1,
+        F3_1Pt44_512 = 0x2,
+        F3_2Pt88_512 = 0x3,
+        F3_20Pt8_512 = 0x4,
+        F3_720_512 = 0x5,
+        F5_360_512 = 0x6,
+        F5_320_512 = 0x7,
+        F5_320_1024 = 0x8,
+        F5_180_512 = 0x9,
+        F5_160_512 = 0xA,
+        RemovableMedia = 0xB,
+        FixedMedia = 0xC,
+        F3_120M_512 = 0xD,
+        F3_640_512 = 0xE,
+        F5_640_512 = 0xF,
+        F5_720_512 = 0x10,
+        F3_1Pt2_512 = 0x11,
+        F3_1Pt23_1024 = 0x12,
+        F5_1Pt23_1024 = 0x13,
+        F3_128Mb_512 = 0x14,
+        F3_230Mb_512 = 0x15,
+        F8_256_128 = 0x16,
+        F3_200Mb_512 = 0x17,
+        F3_240M_512 = 0x18,
+        F3_32M_512 = 0x19
+    }
+
+}
--- DataTools.Win32.Disk/Disk/Partition/Gpt/RawGptDisk.cs
+++ DataTools.Win32.Disk/Disk/Partition/Gpt/RawGptDisk.cs
@@ -17,11 +17,9 @@ using System.Security;
 using System.Security.Principal;
 using DataTools.Text;
 using DataTools.Win32.Disk.Partition;
-using DataTools.Win32.Disk.Partition.Mbr;
-using DataTools.Win32.Disk.Partition.Gpt;
 using DataTools.Win32.Memory;
 
-namespace DataTools.Win32
+namespace DataTools.Win32.Disk.Partition.Gpt
 {
     /// <summary>
     /// Raw disk access.  Currently not exposed.
@@ -31,40 +29,7 @@ namespace DataTools.Win32
     public static class RawGptDisk
     {
 
-        /// <summary>
-        /// Disk Geometry Media Types
-        /// </summary>
-        /// <remarks></remarks>
-        public enum MEDIA_TYPE
-        {
-            Unknown = 0x0,
-            F5_1Pt2_512 = 0x1,
-            F3_1Pt44_512 = 0x2,
-            F3_2Pt88_512 = 0x3,
-            F3_20Pt8_512 = 0x4,
-            F3_720_512 = 0x5,
-            F5_360_512 = 0x6,
-            F5_320_512 = 0x7,
-            F5_320_1024 = 0x8,
-            F5_180_512 = 0x9,
-            F5_160_512 = 0xA,
-            RemovableMedia = 0xB,
-            FixedMedia = 0xC,
-            F3_120M_512 = 0xD,
-            F3_640_512 = 0xE,
-            F5_640_512 = 0xF,
-            F5_720_512 = 0x10,
-            F3_1Pt2_512 = 0x11,
-            F3_1Pt23_1024 = 0x12,
-            F5_1Pt23_1024 = 0x13,
-            F3_128Mb_512 = 0x14,
-            F3_230Mb_512 = 0x15,
-            F8_256_128 = 0x16,
-            F3_200Mb_512 = 0x17,
-            F3_240M_512 = 0x18,
-            F3_32M_512 = 0x19
-        }
-
+       
         [StructLayout(LayoutKind.Sequential)]
         public struct DISK_GEOMETRY
         {
@@ -109,13 +74,13 @@ namespace DataTools.Win32
         /// Retrieve the partition table of a GPT-layout disk, manually.
         /// Must be Administrator.
         /// </summary>
-        /// <param name="inf">DiskDeviceInfo object to the physical disk to read.</param>
+        /// <param name="devicePath">Device Path of Disk Device.</param>
         /// <param name="gptInfo">Receives the drive layout information.</param>
         /// <returns>True if successful.</returns>
         /// <remarks></remarks>
-        public static bool ReadRawGptDisk(string devicePath, ref RAW_GPT_DISK gptInfo)
+        public static bool ReadRawGptDisk(string devicePath, out RAW_GPT_DISK? gptInfo)
         {
-
+            gptInfo = null;
 
             // Demand Administrator for accessing a raw disk.
             AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
@@ -225,10 +190,12 @@ namespace DataTools.Win32
                 return false;
 
             // create a new RAW_GPT_DISK structure.
-            gptInfo = new RAW_GPT_DISK();
-            gptInfo.Header = gpt;
-            gptInfo.Partitions = gpp;
-
+            gptInfo = new RAW_GPT_DISK()
+            {
+                Header = gpt,
+                Partitions = gpp
+            };
+            
             // we have succeeded.
             return true;
         }
