d3b7c7d5e13af8a0a509251888a452cf130c430a
Nathaniel Moschkin <theimprobable@hotmail.com>
2022-03-08 21:25:31 -0500

    Add Task Tool

---
+++ DataTools.Win32/Scheduler/TaskTool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.Win32.TaskScheduler;
+
+namespace DataTools.Scheduler
+{
+    /// <summary>
+    /// Tools to add and remove the current assembly from the system startup sequence.
+    /// </summary>
+    public static class TaskTool
+    {
+        /// <summary>
+        /// Enable the current assembly to run on system startup.
+        /// </summary>
+        /// <param name="args">Optional arguments to pass to the program on startup.</param>
+        /// <param name="runLevel">Optional <see cref="TaskRunLevel"/> to use to start the application.</param>
+        public static void EnableOnStartup(string? args = null, TaskRunLevel runLevel = TaskRunLevel.LUA)
+        {
+
+            var task = TaskService.Instance.NewTask();
+            var exec = Process.GetCurrentProcess().MainModule.FileName;
+
+            var asm = Assembly.GetExecutingAssembly().GetName().Name;
+
+            task.Triggers.Add(new LogonTrigger());
+
+            if (args != null)
+                task.Actions.Add(exec, args);
+            else
+                task.Actions.Add(exec);
+
+            task.Principal.RunLevel = runLevel;
+
+            TaskService.Instance.RootFolder.RegisterTaskDefinition(asm, task);
+        }
+
+        /// <summary>
+        /// Remove the current application from the system startup tasks.
+        /// </summary>
+        public static void DisableOnStartup()
+        {
+            var asm = Assembly.GetExecutingAssembly().GetName().Name;
+            TaskService.Instance.RootFolder.DeleteTask(asm, false);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the current assembly is registered to run on system startup.
+        /// </summary>
+        /// <returns></returns>
+        public static bool GetIsEnabled()
+        {
+            var asm = Assembly.GetExecutingAssembly().GetName().Name;
+
+            foreach (var t in TaskService.Instance.RootFolder.Tasks)
+            {
+                if (t.Name == asm) return true;
+            }
+
+            return false;
+        }
+
+    }
+
+}
