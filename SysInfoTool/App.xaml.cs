954ffccd87d47936b9c8ea8d3aac95d4dcc94339
Nathaniel Moschkin <theimprobable@hotmail.com>
2022-04-29 20:06:06 -0400

    Preview turned out to be a bad idea...

--- SysInfoTool/App.xaml.cs
+++ SysInfoTool/App.xaml.cs
@@ -7,6 +7,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace SysInfoTool
 {
@@ -15,5 +18,12 @@ namespace SysInfoTool
     /// </summary>
     public partial class App : Application
     {
+
+
+        public App() : base()
+        {
+            AppCenter.Start("f6f17897-33f6-4910-8fc5-9638de742b77",
+                   typeof(Analytics), typeof(Crashes));
+        }
     }
 }
