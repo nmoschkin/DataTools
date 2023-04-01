fa9444abd3c8232cc4e484f8ffac2a5a712d1d34
Nathaniel Moschkin <nathaniel.moschkin@precise-soft.com>
2023-03-31 22:09:23 -0400

    Making test program more elaborate.

--- Apps/TestDisks/Program.cs
+++ Apps/TestDisks/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Diagnostics;
+using System.Linq;
+using DataTools.Win32.Disk.Partition;
 using DataTools.Win32.Disk.Partition.Gpt;
 using DataTools.Win32.Disk.Partition.Mbr;
 
@@ -14,8 +16,19 @@ namespace TestNetwork
 
             foreach (var disk in disks)
             {
+                Console.WriteLine();
+                Console.WriteLine("Disk: " + disk.ToString());
+                Console.WriteLine("Is Virtual: " + (disk.VirtualDisk != null).ToString());
+
+                if (disk.VirtualDisk?.BackingStore != null)
+                {
+                    Console.WriteLine("Virtual Disk Files");
+                    Console.WriteLine("------------------");
+                    Console.WriteLine(string.Join("\r\n", disk.VirtualDisk.BackingStore));
+                    Console.WriteLine("------------------");
+                }
                 Console.WriteLine("Physical Disk: " + disk.BusReportedDeviceDesc);
-                Console.WriteLine("Partition Style: " + disk.DiskLayout?.ToString() ?? "None");
+                Console.WriteLine("Partition Style: " + disk.DiskLayout?.ToString().ToUpperInvariant() ?? "None");
 
                 if (disk.DiskLayout != null)
                 {
@@ -27,7 +40,16 @@ namespace TestNetwork
                         if (gptInfo?.Header.IsValid ?? false)
                         {
                             Console.WriteLine("Successfully read raw GPT disk.  Total logical partitions: " + gptInfo?.Partitions.Length.ToString());
+
+                            var c = 1;
+                            foreach (var pt in gptInfo?.Partitions)
+                            {
+                                var pc = pt.PartitionCode;
+                                Console.WriteLine($"Partition {c++}: {pc.Name}");
+                            }
                         }
+
+                        
                     }
                     else if (disk.DiskLayout.PartitionStyle == DataTools.Win32.Disk.Partition.PartitionStyle.Mbr)
                     {
@@ -37,8 +59,31 @@ namespace TestNetwork
                             RawMbrDisk.ReadRawMbrDisk(disk.DevicePath, disk.SectorSize, out mbrInfo);
 
                             Console.WriteLine("Successfully read raw MBR disk.  Total logical partitions: " + mbrInfo?.PartitionCount.ToString());
+                            if (mbrInfo != null)
+                            {
+
+                                var c = 1;
+
+                                foreach (var pt in mbrInfo?.Mbr.PartTable)
+                                {
+                                    var pc = pt.PartType;
+                                    Console.WriteLine($"Partition {c++}: {PartitionCodeInfo.FindByCode(pt.PartType).First().Name}");
+                                }
+
+                                foreach (var pt in mbrInfo?.Extended)
+                                {
+                                    var pc = pt.PartType;
+                                    Console.WriteLine($"Partition {c++}: {PartitionCodeInfo.FindByCode(pt.PartType).First().Name}");
+                                }
+
+                            }
+
                         }
-                        catch { }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+
                     }
                 }
             }
